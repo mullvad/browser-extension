2c07c334a8ddca35d85a12ed95782918710a1f75
rui hildt <rui@mullvad.net>
2021-12-01 10:59:06 +0100

    Add servers to storage

---
+++ src/helpers/localStorage.ts
@@ -0,0 +1,40 @@
+import { Servers } from './servers';
+
+export const localStorage = {
+  servers: createStorageMethodsForKey<Servers>('servers', {}),
+};
+
+function createStorageMethodsForKey<T>(key: string, defaultValue: T) {
+  return {
+    set: getSetter<T>(key),
+    get: getGetter<T>(key, defaultValue),
+    remove: getRemover(key),
+  };
+}
+
+function getSetter<T>(key: string): (value: T) => Promise<void> {
+  return (value: T) => {
+    return browser.storage.local.set({ [key]: value });
+  };
+}
+
+function getGetter<T>(key: string, defaultValue: T): () => Promise<T> {
+  return async () => {
+    const value = (await browser.storage.local.get(key))[key] as T;
+    if (typeof value === 'undefined') {
+      return defaultValue;
+    } else {
+      return value;
+    }
+  };
+}
+
+function getRemover(key: string): () => Promise<void> {
+  return () => browser.storage.local.remove(key);
+}
+
+// export function cleanObject(obj: any): any {
+//   // Clean object from Vue getter/setter
+//   // Useful when logging or before saving to storage
+//   return JSON.parse(JSON.stringify(obj));
+// }
---
+++ src/helpers/servers.ts
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { localStorage } from './localStorage';
+
+export interface Server {
+  active: boolean;
+  city_code: string;
+  city_name: string;
+  country_code: string;
+  country_name: string;
+  hostname: string;
+  ipv4_addr_in: string;
+  ipv6_addr_in: string;
+  multihop_port: number;
+  network_port_speed: number;
+  owned: boolean;
+  provider: string;
+  pubkey: string;
+  socks_name: string;
+}
+
+export interface Country {
+  [city: string]: Server[];
+}
+
+export interface Servers {
+  [country: string]: Country;
+}
+
+/**
+ * Fetch servers list and save it to storage
+ */
+export const serversToStorage = async function () {
+  try {
+    const servers: Servers = {};
+
+    const { data } = await axios.get('https://api.mullvad.net/www/relays/wireguard/');
+
+    data
+      .filter((server: Server) => server.active == true)
+      .forEach((server: Server) => {
+        const countryName: string = server.country_name;
+        const cityName: string = server.city_name;
+
+        // Create country object if not present
+        if (!(countryName in servers)) {
+          servers[countryName] = {};
+        }
+        // Create a city array if not present
+        if (!(cityName in servers[countryName])) {
+          servers[countryName][cityName] = [];
+        }
+        // Add server to servers
+        servers[countryName][cityName].push(server);
+      });
+
+    localStorage.servers.set(servers);
+  } catch (error) {
+    console.log(`Couldn't get the servers list`, error);
+  }
+};
