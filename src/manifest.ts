6ad5c5700649fb1a235fc6d48991297c35cf6445
Hank <hank@mullvad.net>
2021-12-02 15:11:34 +0100

    Use Mullvad icon

--- src/manifest.ts
+++ src/manifest.ts
@@ -5,7 +5,7 @@ import { isDev, port, r } from '../scripts/utils';
 
 export async function getManifest() {
   const pkg = (await fs.readJSON(r('package.json'))) as typeof PkgType;
-
+  
   // update this file to update this manifest.json
   // can also be conditional based on your need
   const manifest: Manifest.WebExtensionManifest = {
@@ -14,7 +14,11 @@ export async function getManifest() {
     version: pkg.version,
     description: pkg.description,
     browser_action: {
-      default_icon: './assets/icon-512.png',
+      default_icon: {
+        "16": "assets/logo.svg",
+        "48": "assets/logo.svg",
+        "96": "assets/logo.svg"
+      },
       default_popup: './dist/popup/index.html',
     },
     options_ui: {
@@ -27,9 +31,11 @@ export async function getManifest() {
       persistent: false,
     },
     icons: {
-      16: './assets/icon-512.png',
-      48: './assets/icon-512.png',
-      128: './assets/icon-512.png',
+      '16': 'assets/logo.svg',
+      '48': 'assets/logo.svg',
+      '96': 'assets/logo.svg',
+      '128': 'assets/logo.svg',
+      '256': 'assets/logo.svg',
     },
     permissions: ['*://*.mullvad.net/*', 'management', 'storage', 'privacy', 'proxy'],
     content_scripts: [
@@ -40,17 +46,17 @@ export async function getManifest() {
     ],
     web_accessible_resources: ['dist/contentScripts/style.css'],
   };
-
+  
   if (isDev) {
     // for content script, as browsers will cache them for each reload,
     // we use a background script to always inject the latest version
     // see src/background/contentScriptHMR.ts
     delete manifest.content_scripts;
     manifest.permissions?.push('webNavigation');
-
+    
     // this is required on dev for Vite script to load
     manifest.content_security_policy = `script-src \'self\' http://localhost:${port}; object-src \'self\'`;
   }
-
+  
   return manifest;
 }
