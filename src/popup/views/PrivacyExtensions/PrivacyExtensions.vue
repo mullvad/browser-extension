6a90055c183aa1ab65e1e627063521d3a51c6c4e
rui hildt <rui@mullvad.net>
2021-12-02 10:58:44 +0100

    Add Home page

--- src/popup/views/PrivacyExtensions/PrivacyExtensions.vue
+++ src/popup/views/PrivacyExtensions/PrivacyExtensions.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
-
 import { Extension, sortExtensions } from '@/helpers/extensions';
 import { storageLocal } from '@/helpers/storageLocal';
 import PrivacyExtension from '@/popup/views/PrivacyExtensions/PrivacyExtension.vue';
 
 const extensions = ref<Extension[]>([]);
+
 storageLocal.extensions.get().then((ext) => {
   extensions.value = sortExtensions(ext);
 });
@@ -13,16 +13,12 @@ const updateRecommendations = async () => {
   const ext = await storageLocal.extensions.get();
   extensions.value = sortExtensions(ext);
 };
-
 </script>
 <template>
   <section>
     <p>Here's a list of recommended third party extensions to improve your privacy</p>
     <div v-for="extension in extensions" :key="extension.id">
-      <PrivacyExtension
-        :extension="extension"
-        @update-recommendations="updateRecommendations"
-      />
+      <PrivacyExtension :extension="extension" @update-recommendations="updateRecommendations" />
     </div>
   </section>
 </template>
