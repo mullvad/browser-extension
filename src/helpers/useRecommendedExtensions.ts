e7040f72dda51f3c0a1db776d70cbb1547cd025e
Hank <hank@mullvad.net>
2021-12-08 10:42:38 +0100

    Refactor to use computed()

--- src/helpers/useRecommendedExtensions.ts
+++ src/helpers/useRecommendedExtensions.ts
@@ -1,16 +1,14 @@
 import { Extension } from '@/helpers/extensions';
-import { storageLocal } from '@/helpers/storageLocal';
 import { Ref } from 'vue-demi';
+import useStoredExtensions from '@/helpers/useStoredExtensions';
 
 const useRecommendedExtensions = (): Ref<Extension[]> => {
-  const recommendedExtensions = ref<Extension[]>([]);
-  storageLocal.extensions.get().then((storedExtensions) => {
-    recommendedExtensions.value = storedExtensions.filter(
+  const extensions = useStoredExtensions();
+  return computed(() =>
+    extensions.value.filter(
       (extension) => !extension.ignored || !extension.enabled || !extension.installed,
-    );
-  });
-
-  return recommendedExtensions;
+    ),
+  );
 };
 
 export default useRecommendedExtensions;
