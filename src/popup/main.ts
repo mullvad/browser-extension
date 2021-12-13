b191a21ffc268562f383ad47d90e24add9436a1d
Hank <hank@mullvad.net>
2021-12-13 12:10:11 +0100

    Remove the base tag thingy as it breaks the images

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,14 +1,9 @@
 import { createApp } from 'vue';
 import { createRouter, createMemoryHistory } from 'vue-router';
-import browser from 'webextension-polyfill';
 
 import routes from '@/popup/routes';
 import App from './Popup.vue';
 import '../styles';
-
-const baseElem = document.createElement('base');
-baseElem.href = __DEV__ ? 'http://localhost:3303' : browser.runtime.getURL('/');
-document.head.appendChild(baseElem);
 
 const router = createRouter({
   history: createMemoryHistory(),
