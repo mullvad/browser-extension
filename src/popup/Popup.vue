6628210c8092066c97bb4bb7abc18b5289bcc9ff
Hank <hank@mullvad.net>
2021-12-14 19:12:01 +0100

    Prettier

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -24,6 +24,6 @@ const logoUrl = '/assets/icon.svg';
 </template>
 <style scoped>
 header {
-  box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px, rgba(0, 0, 0, 0.05) 0px 1px 4px 0px, rgba(0, 0, 0, 0.05) 0px 2px 8px 0px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0 1px 2px 0, rgba(0, 0, 0, 0.05) 0 1px 4px 0, rgba(0, 0, 0, 0.05) 0 2px 8px 0;
 }
 </style>
