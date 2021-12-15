542996383f0c0fc67a9ee83ff5aaf49ae1158a3d
Hank <hank@mullvad.net>
2021-12-15 16:43:03 +0100

    Add back navigation

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,19 +1,21 @@
 <script lang="ts" setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+const path = computed(() => {
+  return useRoute().path;
+});
 const logoUrl = '/assets/icon.svg';
 </script>
 <template>
   <main class="w-[500px]">
     <header class="px-4 py-2 min-h-14 flex justify-between items-center sticky top-0 z-1">
       <div class="flex items-center">
-        <img
-          :src="logoUrl"
-          class="w-8 h-8"
-          alt="Mullvad Logo"
-        >
+        <img v-if="path === '/'" :src="logoUrl" class="w-8 h-8" alt="Mullvad Logo" />
+        <router-link v-else to="/" class="flex w-[32px]">
+          <FeArrowLeft />
+        </router-link>
         <div class="mx-4">
-          <h1 class="uppercase text-2xl font-bold">
-            Mullvad Privacy Companion
-          </h1>
+          <h1 class="uppercase text-2xl font-bold">Mullvad Privacy Companion</h1>
         </div>
       </div>
     </header>
@@ -24,6 +26,7 @@ const logoUrl = '/assets/icon.svg';
 </template>
 <style scoped>
 header {
-  box-shadow: rgba(0, 0, 0, 0.05) 0 1px 2px 0, rgba(0, 0, 0, 0.05) 0 1px 4px 0, rgba(0, 0, 0, 0.05) 0 2px 8px 0;
+  box-shadow: rgba(0, 0, 0, 0.05) 0 1px 2px 0, rgba(0, 0, 0, 0.05) 0 1px 4px 0,
+    rgba(0, 0, 0, 0.05) 0 2px 8px 0;
 }
 </style>
