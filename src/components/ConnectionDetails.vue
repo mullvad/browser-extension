a7748787a19234a0ae6c355c6a21c6cafe78d2fc
Hank <hank@mullvad.net>
2021-12-15 10:55:11 +0100

    Add instructions on how to enable Proxy settings

--- src/components/ConnectionDetails.vue
+++ src/components/ConnectionDetails.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
+import { extension } from 'webextension-polyfill';
 import { Connection } from '@/helpers/connCheck';
 import ProxyButton from '@/components/ProxyButton.vue';
+import { asyncComputed } from '@vueuse/core';
 
-defineProps<{ connection: Connection; }>();
+defineProps<{ connection: Connection }>();
+const showProxySection = asyncComputed(() => extension.isAllowedIncognitoAccess());
 </script>
 <template>
   <h1 class="text-xl pb-1 pt-4">Connection</h1>
@@ -52,12 +55,40 @@ defineProps<{ connection: Connection; }>();
   </div>
   <details>
     <summary>Show Proxy</summary>
-    <p>{{ connection.protocol }}</p>
-    <ProxyButton />
+    <div v-if="showProxySection">
+      <p>{{ connection.protocol }}</p>
+      <ProxyButton />
+    </div>
+    <div v-else>
+      <p>Please allow <em>Run in Private Windows</em>:</p>
+      <ol>
+        <li>
+          <p class="flex items-center">
+            Click the&nbsp;<em class="inline-flex"><IcRoundMenu /></em>&nbsp;icon in the top right
+            corner of the browser
+          </p>
+        </li>
+        <li>Click <em>Add-ons and themes</em></li>
+        <li>Click <em>Extensions</em> in the left hand sidebar</li>
+        <li>Click <em>Mullvad Privacy Companion</em></li>
+        <li>Make sure that <em>Run in Private Windows</em> is set to <em>Allow</em></li>
+      </ol>
+    </div>
   </details>
 </template>
 <style scoped>
+details {
+  padding-left: 1rem;
+}
 summary {
   cursor: pointer;
+  margin-left: -1rem;
+}
+em {
+  color: hsl(211deg 21% 78%);
+}
+ol {
+  margin-left: 1rem;
+  list-style-type: decimal;
 }
 </style>
