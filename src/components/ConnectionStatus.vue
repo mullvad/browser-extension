7f708d2e852cc2dd6cfdb01a190853e9948d460f
Hank <hank@mullvad.net>
2021-12-20 11:15:08 +0100

    Use correct wording when not using Proxy

--- src/components/ConnectionStatus.vue
+++ src/components/ConnectionStatus.vue
@@ -5,11 +5,12 @@ import IconLabel from '@/components/IconLabel.vue';
 const props = defineProps<{ connected: boolean; protocol?: string }>();
 const { protocol } = toRefs(props);
 const isUsingProxy = computed(() => protocol?.value?.startsWith('SOCKS') ?? false);
+const proxyText = computed(() => isUsingProxy.value ? 'Using Proxy' : 'Not Using Proxy');
 </script>
 <template>
   <div v-if="connected" class="mt-8">
     <IconLabel :state="connected" text="Using Mullvad VPN" />
-    <IconLabel :state="isUsingProxy" text="Using Proxy" />
+    <IconLabel :state="isUsingProxy" :text="proxyText" />
   </div>
 </template>
 <style scoped>
