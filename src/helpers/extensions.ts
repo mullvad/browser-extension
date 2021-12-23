ec1665b5ce62ed5a24ef4a0aa9545d91355a2d76
Hank <hank@mullvad.net>
2021-12-23 13:56:36 +0100

    Remove unused function

--- src/helpers/extensions.ts
+++ src/helpers/extensions.ts
@@ -27,10 +27,6 @@ const onDisable = async (extensionInfo: ExtensionInfo) => {
   await updateExtConfig(extensionInfo, { enabled: false });
 };
 
-export const onIgnore = async (extensionInfo: ExtensionInfo, status: boolean) => {
-  await updateExtConfig(extensionInfo, { ignored: status });
-};
-
 export const initExtensions = () => {
   // Add listener on extension action
   addExtListeners();
