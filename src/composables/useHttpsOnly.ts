98864c9b375012290977eee56d6a3a755d0d926f
Hank <hank@mullvad.net>
2022-01-18 20:29:20 +0100

    Use `privacy` from webextension-polyfill

--- src/composables/useHttpsOnly.ts
+++ src/composables/useHttpsOnly.ts
@@ -1,5 +1,7 @@
+import { privacy } from 'webextension-polyfill';
+
 const useHttpsOnly = async () => {
-  const { value: httpsOnlyMode } = await browser.privacy.network.httpsOnlyMode.get({});
+  const { value: httpsOnlyMode } = await privacy.network.httpsOnlyMode.get({});
   // Possible values returned are: "never", "always", "private_browsing"
   return httpsOnlyMode === 'always';
 };
