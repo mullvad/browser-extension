0d4f0cc1f87a5a5351684f51e14589e61d7a8cd0
Hank <hank@mullvad.net>
2022-01-19 18:11:38 +0100

    Add topbar menu, with About page containing version number

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -4,6 +4,8 @@ import { useRoute, useRouter } from 'vue-router';
 import { NScrollbar } from 'naive-ui';
 
 import FeArrowLeft from '~icons/fe/arrow-left';
+
+import TopBarMenu from '@/components/TopBarMenu.vue';
 const logoUrl = '/assets/icon.svg';
 
 const path = computed(() => {
@@ -15,15 +17,18 @@ const pageTitle = computed(() => currentRoute.value.meta.title ?? 'Mullvad Priva
 
 <template>
   <main class="w-[500px] h-[600px]">
-    <header class="px-4 py-2 min-h-14 flex justify-between items-center sticky top-0 z-1">
-      <div class="flex items-center">
-        <img v-if="path === '/'" :src="logoUrl" class="w-8 h-8" alt="Mullvad Logo" />
-        <router-link v-else to="/" class="flex w-[32px]">
-          <FeArrowLeft />
-        </router-link>
-        <div class="mx-4">
-          <h1 class="uppercase text-2xl font-bold">{{ pageTitle }}</h1>
+    <header class="px-4 py-2 min-h-14 flex items-center sticky top-0 z-1">
+      <div class="flex flex-grow items-center justify-between">
+        <div class="flex items-center">
+          <img v-if="path === '/'" :src="logoUrl" class="w-8 h-8" alt="Mullvad Logo" />
+          <router-link v-else to="/" class="flex w-[32px]">
+            <FeArrowLeft />
+          </router-link>
+          <div class="mx-4">
+            <h1 class="uppercase text-2xl font-bold">Mullvad Privacy Companion</h1>
+          </div>
         </div>
+        <TopBarMenu />
       </div>
     </header>
 
--- src/popup/routes.ts
+++ src/popup/routes.ts
@@ -1,10 +1,14 @@
 import { RouteRecordRaw } from 'vue-router';
 
 import Home from '@/popup/views/Home.vue';
+import About from '@/popup/views/About.vue';
+import Location from '@/popup/views/Location.vue';
 import PrivacyRecommendations from '@/popup/views/PrivacyRecommendations.vue';
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', component: Home, meta: { title: 'Mullvad Privacy Companion' } },
+  { path: '/about', component: About },
+  { path: '/location', component: Location },
   {
     path: '/privacy-recommendations',
     component: PrivacyRecommendations,
