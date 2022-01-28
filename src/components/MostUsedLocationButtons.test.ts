28b18ba825512dfabbfeccbb7b4bfc1e22eaff8e
Hank <hank@mullvad.net>
2022-01-28 16:27:30 +0100

    Add MostUsedLocationButtons test

---
+++ src/components/MostUsedLocationButtons.test.ts
@@ -0,0 +1,49 @@
+import { mount } from '@vue/test-utils';
+import MostUsedLocationButtons from '@/components/MostUsedLocationButtons.vue';
+
+import useHistoricConnections from '@/composables/useHistoricConnections/useHistoricConnections';
+import Button from '@/components/Buttons/Button.vue';
+
+jest.mock('@/composables/useHistoricConnections/useHistoricConnections', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+describe('MostUsedLocationButtons', () => {
+  it('should render one button', () => {
+    (useHistoricConnections as jest.Mock).mockReturnValueOnce({
+      mostUsed: [{ country: 'Argentina' }],
+      getLabel: ({ country }: { country: string }) => country,
+    });
+    const wrapper = mount(MostUsedLocationButtons, { props: { selectLocation: jest.fn() } });
+    const buttons = wrapper.findAllComponents(Button);
+
+    expect(buttons).toHaveLength(1);
+    expect(buttons.at(0)?.text()).toMatch(/argentina/i);
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('should render three buttons', () => {
+    (useHistoricConnections as jest.Mock).mockReturnValueOnce({
+      mostUsed: [
+        { country: 'Sweden' },
+        { country: 'Norway' },
+        { country: 'Iceland' },
+        { country: 'Denmark' },
+        { country: 'France' },
+        { country: 'Spain' },
+      ],
+      getLabel: ({ country }: { country: string }) => country,
+    });
+    const wrapper = mount(MostUsedLocationButtons, { props: { selectLocation: jest.fn() } });
+    const buttons = wrapper.findAllComponents(Button);
+
+    expect(buttons).toHaveLength(3);
+    expect(buttons.at(0)?.text()).toMatch(/sweden/i);
+    expect(buttons.at(1)?.text()).toMatch(/norway/i);
+    expect(buttons.at(2)?.text()).toMatch(/iceland/i);
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
