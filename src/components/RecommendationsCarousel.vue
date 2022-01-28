f5bcf78531cf48dc2bb936eae91307c94c6b44c1
Hank <hank@mullvad.net>
2022-01-28 11:50:48 +0100

    Add test for RecommendationCarousel

--- src/components/RecommendationsCarousel.vue
+++ src/components/RecommendationsCarousel.vue
@@ -17,7 +17,7 @@ const { activeRecommendations } = useRecommendations();
   <h1 class="text-sm pb-1">Privacy recommendations</h1>
 
   <div v-if="activeRecommendations.length === 0">
-    <p class="text-white text-lg">Sweet! You have taken action on all recommendations.</p>
+    <p class="text-white text-lg" data-test="success-message">Sweet! You have taken action on all recommendations.</p>
     <router-link class="hover:text-white" to="privacy-recommendations">
       See all Privacy Recommendations
     </router-link>
@@ -82,7 +82,7 @@ const { activeRecommendations } = useRecommendations();
     </n-carousel>
 
     <div class="text-right pt-2 mr-2">
-      <router-link class="hover:text-white" to="privacy-recommendations">Show all</router-link>
+      <router-link class="hover:text-white" to="privacy-recommendations" data-test="show-all-link">Show all</router-link>
     </div>
   </div>
 </template>
