e282bb1bcdfda4743c5a2188d04b121a0e925222
Hank <hank@mullvad.net>
2022-01-31 16:37:27 +0100

    Add ExternalLinkIconLabel

---
+++ src/components/ExternalLinkIconLabel.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import FeLinkExternal from '@/components/Icons/FeLinkExternal.vue';
+
+defineProps<{ text: string }>();
+</script>
+
+<template>
+  <span> {{ text }} <FeLinkExternal class="text-info ml-2" /> </span>
+</template>
+
+<style scoped>
+span {
+  display: flex;
+  align-items: center;
+}
+</style>
