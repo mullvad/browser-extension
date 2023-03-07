7e1e3beedaf08ad6571d52d69d4297cf3170adbe
rui hildt <rui@mullvad.net>
2023-03-07 15:26:38 +0100

    Add Settings and move Recommendations as submenu

---
+++ src/popup/headers/HomeHeader.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { NAvatar, NIcon } from 'naive-ui';
+
+import FeCog from '@/components/Icons/FeCog.vue';
+
+const logoUrl = '/assets/icon.svg';
+
+const { currentRoute } = useRouter();
+
+const pageTitle = computed(() => currentRoute.value.meta.title);
+</script>
+
+<template>
+  <div class="flex flex-grow items-center justify-between">
+    <div class="flex items-center">
+      <n-avatar :src="logoUrl" alt="Mullvad Logo" />
+    </div>
+    <div class="mx-4">
+      <h1 class="text-2xl font-semibold">{{ pageTitle }}</h1>
+    </div>
+    <router-link to="/settings" class="flex">
+      <n-icon size="24">
+        <FeCog />
+      </n-icon>
+    </router-link>
+  </div>
+</template>
---
+++ src/popup/headers/SettingsHeader.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { NAvatar, NIcon } from 'naive-ui';
+
+import FeArrowLeft from '@/components/Icons/FeArrowLeft.vue';
+
+const { currentRoute } = useRouter();
+
+const pageTitle = computed(() => currentRoute.value.meta.title);
+</script>
+
+<template>
+  <div class="flex flex-grow items-center justify-between">
+    <div class="flex items-center">
+      <n-avatar>
+        <router-link to="/">
+          <n-icon>
+            <FeArrowLeft />
+          </n-icon>
+        </router-link>
+      </n-avatar>
+    </div>
+    <div class="mx-4">
+      <h1 class="text-2xl font-semibold">{{ pageTitle }}</h1>
+    </div>
+    <div></div>
+  </div>
+</template>
