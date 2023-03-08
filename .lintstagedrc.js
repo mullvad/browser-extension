3ce80a2bd4c8f1b6635207fe47494f4a68c171c8
rui hildt <rui@mullvad.net>
2023-03-08 15:19:44 +0100

    Remove jest from pre-commit hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,ts,vue}': ['eslint', 'prettier --write', 'jest --bail --findRelatedTests'],
+  '*.{js,ts,vue}': ['eslint', 'prettier --write'],
   '*.{ts,vue}': [() => 'tsc --noEmit'],
   '*.{css,scss,vue}': 'stylelint',
 };
