a5d99b385575c909044d0edd74c45c6d34f673a0
rui hildt <rui@mullvad.net>
2023-03-10 15:49:27 +0100

    Improve connection layout

--- src/components/ConnectionDetails/AdvancedDns.vue
+++ src/components/ConnectionDetails/AdvancedDns.vue
@@ -4,22 +4,20 @@ import MuSpinner from '@/components/Icons/MuSpinner.vue';
 
 import useCheckDnsLeaks from '@/composables/useCheckDnsLeaks';
 
-const { dnsServers, isLoading: isGettingDns, isError } = useCheckDnsLeaks();
+const { dnsServers, isLoading, isError } = useCheckDnsLeaks();
 </script>
 
 <template>
-  <h4 class="font-semibold inline-block">DNS Servers</h4>
-
-  <div v-if="isGettingDns">
+  <div v-if="isLoading">
     <div class="flex items-center">
-      Getting DNS Servers
+      Determining DNS Servers
       <MuSpinner class="ml-2" />
     </div>
   </div>
 
   <div v-else-if="isError" class="pl-2">Could not determine DNS servers</div>
 
-  <div v-else class="text-white pl-2">
+  <div v-else class="text-white ml-35px">
     <div v-for="dnsServer in dnsServers" :key="dnsServer.ip">
       <IconLabel
         :text="`${dnsServer.ip} (${
@@ -28,7 +26,7 @@ const { dnsServers, isLoading: isGettingDns, isError } = useCheckDnsLeaks();
           dnsServer.organization ||
           'unknown'
         })`"
-        :type="dnsServer.mullvad_dns_hostname ? 'success' : 'leak'"
+        :type="dnsServer.mullvad_dns_hostname ? 'check' : 'leak'"
       />
     </div>
   </div>
--- src/components/ConnectionDetails/AdvancedInfo.vue
+++ src/components/ConnectionDetails/AdvancedInfo.vue
@@ -7,24 +7,22 @@ const { connection } = inject(ConnectionKey, defaultConnection);
 </script>
 
 <template>
-  <div>
-    <h4 class="font-semibold inline-block">IP</h4>
-    {{ connection.ip }}
-  </div>
-  <div v-if="connection.ipv6">
-    <h4 class="font-semibold inline-block">IPv6</h4>
-    {{ connection.ipv6 }}
-  </div>
-  <div>
-    <h4 class="font-semibold inline-block">Provider</h4>
-    {{ connection.provider }}
-  </div>
-  <div v-if="connection.isMullvad">
-    <h4 class="font-semibold inline-block">Server</h4>
-    {{ connection.server }}
-  </div>
-  <div>
-    <h4 class="font-semibold inline-block">IP</h4>
-    {{ connection.ip }}
+  <div class="ml-35px">
+    <div>
+      <h4 class="font-semibold inline-block">IP</h4>
+      <span class="ml-2">{{ connection.ip }}</span>
+    </div>
+    <div v-if="connection.ipv6">
+      <h4 class="font-semibold inline-block">IPv6</h4>
+      <span class="ml-2">{{ connection.ipv6 }}</span>
+    </div>
+    <div>
+      <h4 class="font-semibold inline-block">Provider</h4>
+      <span class="ml-2">{{ connection.provider }}</span>
+    </div>
+    <div v-if="connection.isMullvad">
+      <h4 class="font-semibold inline-block">Server</h4>
+      <span class="ml-2">{{ connection.server }}</span>
+    </div>
   </div>
 </template>
