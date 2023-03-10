2997fd7707b455d5140101a7c2f33507c0775920
rui hildt <rui@mullvad.net>
2023-03-10 13:52:41 +0100

    Restructure connection informations

---
+++ src/components/ConnectionDetails/AdvancedInfo.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { inject } from 'vue';
+
+import { ConnectionKey, defaultConnection } from '@/composables/useConnection';
+
+const { connection } = inject(ConnectionKey, defaultConnection);
+</script>
+
+<template>
+  <div>
+    <h4 class="font-semibold inline-block">IP</h4>
+    {{ connection.ip }}
+  </div>
+  <div v-if="connection.ipv6">
+    <h4 class="font-semibold inline-block">IPv6</h4>
+    {{ connection.ipv6 }}
+  </div>
+  <div>
+    <h4 class="font-semibold inline-block">Provider</h4>
+    {{ connection.provider }}
+  </div>
+  <div v-if="connection.isMullvad">
+    <h4 class="font-semibold inline-block">Server</h4>
+    {{ connection.server }}
+  </div>
+  <div>
+    <h4 class="font-semibold inline-block">IP</h4>
+    {{ connection.ip }}
+  </div>
+</template>
