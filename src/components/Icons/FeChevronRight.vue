bd8e16ac92ae466eccdb0fb61f763c222a19212f
rui hildt <rui@mullvad.net>
2023-03-13 11:25:11 +0100

    Format Icon with latest eslint rules

--- src/components/Icons/FeChevronRight.vue
+++ src/components/Icons/FeChevronRight.vue
@@ -9,7 +9,6 @@
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
-    class="feather feather-chevron-down"
   >
     <polyline points="9 18 15 12 9 6"></polyline>
   </svg>
