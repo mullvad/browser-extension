5429c7ef90a9d9d92ef6892d9ee505e3a56ffc8c
rui <rui@mullvad.net>
2023-03-15 11:04:47 +0100

    Use ip instead of hostname to connect via socks5

--- src/composables/useHistoricConnections/useHistoricConnections.ts
+++ src/composables/useHistoricConnections/useHistoricConnections.ts
@@ -2,12 +2,22 @@ import { watchEffect } from 'vue';
 import useBrowserStorageLocal from '@/composables/useBrowserStorageLocal';
 import type { HistoricConnection } from '@/composables/useHistoricConnections/HistoricConnections.types';
 
-type StoreSocksProxyUsageProps = { country?: string; city?: string; hostname?: string };
+type StoreSocksProxyUsageProps = {
+  country?: string;
+  city?: string;
+  hostname?: string;
+  ipv4_address?: string;
+};
 type HistoricConnectionsMap = { [key: string]: { count: number; timestamp: number } };
 
 const historicConnections = useBrowserStorageLocal<HistoricConnectionsMap>('connections', {});
 
-const storeSocksProxyUsage = ({ country, city, hostname }: StoreSocksProxyUsageProps) => {
+const storeSocksProxyUsage = ({
+  country,
+  city,
+  hostname,
+  ipv4_address,
+}: StoreSocksProxyUsageProps) => {
   const keys: string[] = [];
   if (country) {
     keys.push(country);
@@ -17,6 +27,9 @@ const storeSocksProxyUsage = ({ country, city, hostname }: StoreSocksProxyUsageP
   }
   if (hostname) {
     keys.push(hostname);
+  }
+  if (ipv4_address) {
+    keys.push(ipv4_address);
   }
   const key = keys.join(',');
   const data = historicConnections.value[key] ?? { count: 0 };
@@ -44,8 +57,8 @@ let sortedByRecentConnections: HistoricConnection[];
 watchEffect(() => {
   sortedByUsageConnections = Object.entries(historicConnections.value)
     .map(([key, { count, timestamp }]) => {
-      const [country, city, hostname] = key.split(',');
-      return { country, city, hostname, count, timestamp };
+      const [country, city, hostname, ipv4_address] = key.split(',');
+      return { country, city, hostname, ipv4_address, count, timestamp };
     })
     .sort((a, b) => b.count - a.count || b.timestamp - a.timestamp);
 
--- src/helpers/getRandomSocksProxy.ts
+++ src/helpers/getRandomSocksProxy.ts
@@ -1,16 +1,16 @@
 import type { Country } from '@/composables/useSocksProxies';
 
 type Props = {
-  socksProxies: Country[] | undefined
+  socksProxies: Country[] | undefined;
   country: string;
   city?: string;
-}
+};
 
 const getRandomSocksProxy = ({ socksProxies, country, city }: Props) => {
   if (!socksProxies || !socksProxies.length) {
     throw new Error('No proxies to choose from');
   }
-  
+
   const theCountry = socksProxies.filter((c) => c.country === country)[0];
   let proxies;
   if (!city) {
@@ -22,9 +22,9 @@ const getRandomSocksProxy = ({ socksProxies, country, city }: Props) => {
     proxies = proxyList;
   }
   const index = Math.floor(Math.random() * proxies.length);
-  const { hostname, port } = proxies[index];
-  
-  return { hostname, port };
+  const { hostname, ipv4_address, port } = proxies[index];
+
+  return { hostname, ipv4_address, port };
 };
 
 export default getRandomSocksProxy;
