bce8568edf59ebcc9044f8740cc368696581fa52
rui <rui@mullvad.net>
2023-03-22 19:27:27 +0100

    Clean storage from old settings/recommendations

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,3 +1,4 @@
+import { runtime, storage } from 'webextension-polyfill';
 import { addExtListeners } from '@/helpers/extensions';
 
 import useRecommendations from '@/composables/useRecommendations/useRecommendations';
@@ -11,15 +12,28 @@ if (import.meta.hot) {
 }
 
 const { initWebRTC } = useWebRtc();
+const { cleanOutdated } = useRecommendations();
 
 // Add listeners on extension actions
 addExtListeners();
-
-// Load recommendations settings
-useRecommendations();
 
 // Load socks settings
 useSocksProxy();
 
 // Load webRTC settings
 initWebRTC();
+
+// Cleanup outdated settings from storage from the 0.6.x series
+// TO REMOVE IN THE 0.8.x versions
+runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+  if (temporary) return; // skip during development
+  switch (reason) {
+    // see above
+    case 'update':
+      {
+        await storage.local.remove(['detailsExpanded', 'proxyExpanded']);
+        cleanOutdated();
+      }
+      break;
+  }
+});
