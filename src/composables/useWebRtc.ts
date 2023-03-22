2212fddf0a7383b13e8a52fd12f29dc535edea7d
rui hildt <rui@mullvad.net>
2023-03-22 19:56:01 +0100

    Implement POC for webRTC leak detection

--- src/composables/useWebRtc.ts
+++ src/composables/useWebRtc.ts
@@ -1,4 +1,7 @@
+import { ref } from 'vue';
 import useRecommendations from './useRecommendations/useRecommendations';
+
+const leakedInternalIPs = ref();
 
 const setWebRTC = (isDisabled: boolean) => {
   // Disable WebRTC in Firefox
@@ -17,10 +20,40 @@ const initWebRTC = () => {
   setWebRTC(webRtcRecommendation?.activated ?? true);
 };
 
+const isLeaking = () => {
+  if (!RTCPeerConnection) {
+    // RTC not supported?
+    return false;
+  }
+
+  const pc = new RTCPeerConnection();
+  const leakedHosts = new Set();
+  pc.onicecandidate = (e) => {
+    console.log(e);
+    if (e.candidate) {
+      const host = e.candidate.candidate.split(' ')[4];
+      const isObfuscated = !host || host.endsWith('.local');
+      if (!isObfuscated) {
+        leakedHosts.add(host);
+      }
+    }
+    if (pc.iceGatheringState === 'complete') {
+      console.log('leakedHosts', [...leakedHosts]);
+      // end of gathering
+      leakedInternalIPs.value = [...leakedHosts];
+    }
+  };
+
+  pc.createOffer({ offerToReceiveAudio: true }).then((offer) => pc.setLocalDescription(offer));
+  return true;
+};
+
 const useWebRtc = () => {
   return {
     setWebRTC,
     initWebRTC,
+    isLeaking,
+    leakedInternalIPs,
   };
 };
 
--- src/popup/views/Home.vue
+++ src/popup/views/Home.vue
@@ -5,12 +5,20 @@ import NotificationsCarousel from '@/components/NotificationsCarousel.vue';
 import ConnectionDetails from '@/components/ConnectionDetails/ConnectionDetails.vue';
 
 import useConnection, { ConnectionKey } from '@/composables/useConnection';
+import useWebRtc from '@/composables/useWebRtc';
 
 const { isLoading, connection, isError } = useConnection();
+const { isLeaking, leakedInternalIPs } = useWebRtc();
 provide(ConnectionKey, { connection, isLoading, isError });
 </script>
 
 <template>
+  <div v-if="isLeaking()">
+    <p v-for="ip in leakedInternalIPs" :key="ip">
+      {{ ip }}
+    </p>
+  </div>
+
   <NotificationsCarousel />
   <ConnectionDetails />
 </template>
