b78011fc514cc51fd95a2caaf98161d9b17fc013
rui hildt <rui@mullvad.net>
2023-03-24 19:32:17 +0100

    Add webRTC status/IPs when leaking

--- src/popup/views/Home.vue
+++ src/popup/views/Home.vue
@@ -5,21 +5,13 @@ import NotificationsCarousel from '@/components/NotificationsCarousel.vue';
 import ConnectionDetails from '@/components/ConnectionDetails/ConnectionDetails.vue';
 
 import useConnection, { ConnectionKey } from '@/composables/useConnection';
-import useWebRtc from '@/composables/useWebRtc';
 
 const { isLoading, connection, isError } = useConnection();
-const { webRTCLeaking, webRTCLeakedIPs } = useWebRtc();
 
 provide(ConnectionKey, { connection, isLoading, isError });
 </script>
 
 <template>
-  <div v-if="webRTCLeaking">
-    <p v-for="ip in webRTCLeakedIPs" :key="ip">
-      {{ ip }}
-    </p>
-  </div>
-
   <NotificationsCarousel />
   <ConnectionDetails />
 </template>
