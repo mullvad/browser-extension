ce77ec6c95173a77f2e75f1f9f718aba7c6e3463
rui hildt <rui@mullvad.net>
2023-05-16 14:50:57 +0200

    Remove legacy code to clean local storage

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,7 +1,4 @@
-import { runtime, storage } from 'webextension-polyfill';
 import { addExtListeners } from '@/helpers/extensions';
-
-import useRecommendations from '@/composables/useRecommendations/useRecommendations';
 import useWebRtc from '@/composables/useWebRtc';
 
 // only on dev mode
@@ -11,25 +8,9 @@ if (import.meta.hot) {
 }
 
 const { toggleWebRTC, webRTCStatus } = useWebRtc();
-const { cleanOutdated } = useRecommendations();
 
 // Add listeners on extension actions
 addExtListeners();
 
 // Load webRTC settings from storage
 toggleWebRTC(webRTCStatus.value);
-
-// Cleanup outdated settings from storage from the 0.6.x series
-// TO REMOVE IN THE 0.8.x versions
-runtime.onInstalled.addListener(async ({ reason, temporary }) => {
-  if (temporary) return; // skip during development
-  switch (reason) {
-    // see above
-    case 'update':
-      {
-        await storage.local.remove(['detailsExpanded', 'proxyExpanded']);
-        cleanOutdated();
-      }
-      break;
-  }
-});
