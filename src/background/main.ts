f096dec6887129c2bb175a109a31e82fa3779c3e
rui hildt <rui@mullvad.net>
2023-05-16 17:41:06 +0200

    Complete rebase

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,7 +1,4 @@
-import { runtime, storage } from 'webextension-polyfill';
 import { addExtListeners } from '@/helpers/extensions';
-
-import useRecommendations from '@/composables/useRecommendations/useRecommendations';
 
 // only on dev mode
 if (import.meta.hot) {
@@ -9,22 +6,5 @@ if (import.meta.hot) {
   import('/@vite/client');
 }
 
-const { cleanOutdated } = useRecommendations();
-
 // Add listeners on extension actions
 addExtListeners();
-
-// Cleanup outdated settings from storage from the 0.6.x series
-// TO REMOVE IN THE 0.8.x versions
-runtime.onInstalled.addListener(async ({ reason, temporary }) => {
-  if (temporary) return; // skip during development
-  switch (reason) {
-    // see above
-    case 'update':
-      {
-        await storage.local.remove(['detailsExpanded', 'proxyExpanded']);
-        cleanOutdated();
-      }
-      break;
-  }
-});
