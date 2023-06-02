d513a0bd05cbb72339275ec2388d465b2725512d
rui <rui@mullvad.net>
2023-06-02 16:23:16 +0200

    Leta autologin bug fixes (#162)
    * Format account before attempting login
    * Refactor invalid account error handling
    * Remove uneeded expiry cookie
    * Renew cookie every 23h in a browser session

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,7 +1,13 @@
 import { onMessage } from 'webext-bridge/background';
 
 import { addExtListeners } from '@/helpers/extensions';
-import { DataAccount, initLetaLogin, letaLogin, letaLogout } from '@/helpers/leta';
+import {
+  DataAccount,
+  getMullvadAccount,
+  initLetaLogin,
+  letaLogin,
+  letaLogout,
+} from '@/helpers/leta';
 
 // only on dev mode
 if (import.meta.hot) {
@@ -24,6 +30,22 @@ onMessage<DataAccount>('leta-login', async ({ data }) => {
 onMessage('leta-logout', () => {
   letaLogout();
 });
+
+// Alarm to refresh Mullvad Leta auth cookie
+// The cookie expires after 24h, so renewal is set for every 23h
+browser.alarms.create('leta-cookie-refresh', {
+  delayInMinutes: 1380,
+  periodInMinutes: 1380,
+});
+
+const refreshCookie = async (alarm: browser.alarms.Alarm) => {
+  if (alarm.name === 'leta-cookie-refresh') {
+    const account = await getMullvadAccount();
+    await letaLogin(account);
+  }
+};
+
+browser.alarms.onAlarm.addListener(refreshCookie);
 
 // `browser.cookies` operations are only available in the background context
 export const setCookie = async (cookie: browser.cookies._SetDetails) => {
