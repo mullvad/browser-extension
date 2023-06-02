d611ed733ed56027dc36d7313e1b2d4d2f3a6090
rui <rui@mullvad.net>
2023-06-02 16:23:16 +0200

    Leta autologin bug fixes (#162)
    * Format account before attempting login
    * Refactor invalid account error handling
    * Remove uneeded expiry cookie
    * Renew cookie every 23h in a browser session

--- src/composables/useLeta.ts
+++ src/composables/useLeta.ts
@@ -2,18 +2,17 @@ import { ref } from 'vue';
 import { onMessage, sendMessage } from 'webext-bridge/popup';
 
 import { DataAccount, MullvadAccount } from '@/helpers/leta';
-import { FormatType, formatAccount } from '@/helpers/account';
 import useStore from '@/composables/useStore';
 
 const { mullvadAccount } = useStore();
 const loginError = ref({ error: false, message: '' });
 
-type DataError = {
+export type ErrorMessage = {
   error: boolean;
   message: string;
 };
 
-onMessage<DataError>('login-error', async ({ data }) => {
+onMessage<ErrorMessage>('login-error', async ({ data }) => {
   const { message } = data;
   loginError.value = { error: true, message };
 });
@@ -22,7 +21,7 @@ onMessage<DataAccount>('login-success', async ({ data }) => {
   const { account } = data;
   loginError.value = { error: false, message: '' };
   // Save account to extension storage
-  mullvadAccount.value = formatAccount(account, FormatType.clean);
+  mullvadAccount.value = account;
 });
 
 const login = async (account: MullvadAccount) => {
