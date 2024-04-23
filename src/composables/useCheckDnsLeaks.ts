d19d8059ffe04fc8519c5a92e432f51c0dd2733b
rui hildt <rui@mullvad.net>
2024-04-23 14:03:12 +0200

    Add missing await

--- src/composables/useCheckDnsLeaks.ts
+++ src/composables/useCheckDnsLeaks.ts
@@ -23,7 +23,7 @@ const dnsLeakRequest = async () => {
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
-  return response.json();
+  return await response.json();
 };
 
 const dnsServers = ref([] as DnsServer[]);
