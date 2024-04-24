da6e6d5d5f5d91aae561dcdc60ff7bdd91017979
rui hildt <rui@mullvad.net>
2024-04-24 14:50:33 +0200

    Refresh the extension badge when toggling proxy

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -2,8 +2,10 @@
 import { computed } from 'vue';
 import { NButton, NCollapse, NCollapseItem, NSpace } from 'naive-ui';
 
-import getRandomSocksProxy from '@/helpers/getRandomSocksProxy';
 import LocationTabs from '@/components/LocationTabs.vue';
+
+import { updateTabsProxyBadges } from '@/helpers/browserAction';
+import getRandomSocksProxy from '@/helpers/getRandomSocksProxy';
 
 import useListProxies from '@/composables/useListProxies';
 import useSocksProxy from '@/composables/useSocksProxy';
@@ -41,6 +43,7 @@ const setProxy = (
   } else {
     setGlobalProxy({ country, countryCode, city, hostname, ipv4_address, port });
   }
+  updateTabsProxyBadges();
 };
 
 const clickServer = (
