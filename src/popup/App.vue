2ceb6079b646b551d7f4626bfd051ffd7d194171
rui hildt <rui@mullvad.net>
2024-08-13 13:52:26 +0200

    Consolidate themeOverrides

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,42 +1,13 @@
 <script lang="ts" setup>
 import { provide } from 'vue';
-import { useQueryProvider } from 'vue-query';
-import { NConfigProvider, GlobalThemeOverrides, darkTheme } from 'naive-ui';
+import { NConfigProvider, darkTheme } from 'naive-ui';
 
 import Popup from '@/popup/Popup.vue';
+import { themeOverrides } from '@/styles/themeOverrides';
 import useConnection, { ConnectionKey } from '@/composables/useConnection';
 
 const { isLoading, connection, isError } = useConnection();
-
 provide(ConnectionKey, { connection, isLoading, isError });
-
-useQueryProvider();
-const themeOverrides: GlobalThemeOverrides = {
-  common: {
-    cardColor: 'rgba(41, 77, 115, 0.5)',
-    fontSize: '1rem',
-    lineHeight: '1.4',
-  },
-  Avatar: {
-    color: 'transparent',
-  },
-  Card: {
-    actionColor: 'transparent',
-    borderRadius: '8px',
-  },
-  Drawer: {
-    color: 'var(--dark-blue)',
-  },
-  Switch: {
-    railColorActive: 'var(--success)',
-  },
-  Checkbox: {
-    colorChecked: 'var(--blue)',
-    checkMarkColor: 'var(--success)',
-    border: '1px solid var(--blue)',
-    borderChecked: '1px solid var(--blue)',
-  },
-};
 </script>
 
 <template>
