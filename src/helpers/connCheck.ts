c1540cde06a466d3516c872386a6b876746f5bcd
rui hildt <rui@mullvad.net>
2024-10-17 17:54:23 +0200

    Remove outdated conncheck retry workaround

--- src/helpers/connCheck.ts
+++ src/helpers/connCheck.ts
@@ -4,19 +4,11 @@ import type {
   Ipv4ServerResponse,
 } from '@/helpers/connCheck.types';
 
-/*
-By default, it will retry twice because after connecting/disconnecting to/from Mullvad server, the first request results in a NetworkError and the second is successful.
+export const connCheckIpv4 = async (): Promise<Connection> => {
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), 6000);
 
-It's a workaround for the following bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1706377
-Workaround can be removed when Mullvad Browser 14.0 is released (the bug is fixed!).
-*/
-const MAX_RETRIES = 3;
-
-export const connCheckIpv4 = async (retries = MAX_RETRIES): Promise<Connection> => {
   try {
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), 6000);
-
     const response = await fetch('https://ipv4.am.i.mullvad.net/json', {
       method: 'GET',
       headers: {
@@ -38,12 +30,11 @@ export const connCheckIpv4 = async (retries = MAX_RETRIES): Promise<Connection> 
     };
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
-    if (retries === 1) throw new Error('IPv4 connection check failed.');
-    return connCheckIpv4(retries - 1);
+    throw new Error('IPv4 connection check failed.');
   }
 };
 
-export const connCheckIpv6 = async (retries = MAX_RETRIES): Promise<string | undefined> => {
+export const connCheckIpv6 = async (): Promise<string | undefined> => {
   try {
     const response = await fetch('https://ipv6.am.i.mullvad.net/json', {
       method: 'GET',
@@ -57,7 +48,6 @@ export const connCheckIpv6 = async (retries = MAX_RETRIES): Promise<string | und
     if (__DEV__) {
       console.log(`[conCheck IPv6]: Error trying to get ipv6 data: ${(e as Error).message}`);
     }
-    if (retries === 1) return undefined;
-    return connCheckIpv6(retries - 1);
+    return undefined;
   }
 };
