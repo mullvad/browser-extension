020e41ff0c71f3a67f3618e6c6e46e5a2d1d5bbe
rui hildt <rui@mullvad.net>
2024-11-05 11:06:45 +0100

    Make background persistent.

--- src/manifest.ts
+++ src/manifest.ts
@@ -24,7 +24,7 @@ export async function getManifest() {
     },
     background: {
       page: './dist/background/index.html',
-      persistent: false,
+      persistent: true,
     },
     icons: {
       '16': './assets/mullvad-logo.svg',
