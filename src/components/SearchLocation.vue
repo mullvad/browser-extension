936d7f0a3e5d2cbd5b27f585b6b47d930cc57775
rui hildt <rui@mullvad.net>
2024-12-12 15:09:30 +0100

    Make search input take focus on location selection

--- src/components/SearchLocation.vue
+++ src/components/SearchLocation.vue
@@ -1,10 +1,17 @@
 <script lang="ts" setup>
 import { NInput } from 'naive-ui';
+import { nextTick, onMounted, ref } from 'vue';
 import useSocksProxies from '@/composables/useSocksProxies/useSocksProxies';
 
 const { query } = useSocksProxies();
+const inputRef = ref<HTMLInputElement | null>(null);
+
+onMounted(async () => {
+  await nextTick();
+  inputRef.value?.focus();
+});
 </script>
 
 <template>
-  <NInput v-model:value="query" placeholder="Search" clearable class="mb-3" />
+  <NInput ref="inputRef" v-model:value="query" placeholder="Search" clearable class="mb-3" />
 </template>
