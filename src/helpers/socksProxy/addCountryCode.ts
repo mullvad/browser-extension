ac570bb5df9d0fa3472b53291816dac04cb319f8
rui hildt <rui@mullvad.net>
2025-02-13 12:52:49 +0100

    Show country code in random proxy mode

--- src/helpers/socksProxy/addCountryCode.ts
+++ src/helpers/socksProxy/addCountryCode.ts
@@ -1,17 +1,18 @@
 import { SocksProxy } from '@/helpers/socksProxy/socksProxies.types';
 
+export const addCountryCodeToProxy = (proxy: SocksProxy) => {
+  const { location } = proxy;
+  const countryCode = location.code.substring(0, 2);
+
+  return {
+    ...proxy,
+    location: {
+      ...location,
+      countryCode,
+    },
+  };
+};
+
 export const addCountryCode = (data: SocksProxy[]) => {
-  return data.map((proxy) => {
-    const { location } = proxy;
-
-    const countryCode = location.code.substring(0, 2);
-
-    return {
-      ...proxy,
-      location: {
-        ...location,
-        countryCode,
-      },
-    };
-  });
+  return data.map(addCountryCodeToProxy);
 };
