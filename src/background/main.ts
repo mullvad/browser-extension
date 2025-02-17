c9f94f0e41f395313850b318161e24f909ab1ab7
rui hildt <rui@mullvad.net>
2025-02-17 09:49:43 +0100

    Remove code specific to 0.9.4

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,15 +1,11 @@
 import { addExtensionsListeners } from '@/helpers/extensions';
 import { initProxyListeners } from '@/helpers/proxyListeners';
-import { resetRecommendations } from '@/helpers/recommendations';
 
 // only on dev mode
 if (import.meta.hot) {
   // @ts-expect-error for background HMR
   import('/@vite/client');
 }
-
-// Add listeners to execute on subsequent updates
-resetRecommendations();
 
 // Add listeners on extension actions
 addExtensionsListeners();
