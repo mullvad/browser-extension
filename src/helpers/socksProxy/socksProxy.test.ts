d067603d0a54ab66fa539f2f93b502a11f6452e4
rui hildt <rui@mullvad.net>
2025-02-22 10:20:14 +0100

    Add test for isExtConnCheck

--- src/helpers/socksProxy/socksProxy.test.ts
+++ src/helpers/socksProxy/socksProxy.test.ts
@@ -1,5 +1,7 @@
 import { it, describe, expect } from 'vitest';
-import { isLocalOrReservedIP } from '@/helpers/socksProxy/socksProxy';
+
+import { isExtConnCheck, isLocalOrReservedIP } from '@/helpers/socksProxy/socksProxy';
+import { RequestDetails } from './socksProxy.types';
 
 vi.mock('@/helpers/getRandomSessionProxy', () => ({
   browserStorage: {
@@ -39,5 +41,89 @@ describe('isLocalOrReservedIP', () => {
 
   it('should return false when IP address is not provided', () => {
     expect(isLocalOrReservedIP('')).toBeFalsy();
+  });
+});
+
+describe('isExtConnCheck', () => {
+  const baseDetails: RequestDetails = {
+    requestId: '5979',
+    url: '',
+    method: 'GET',
+    type: 'xmlhttprequest',
+    fromCache: false,
+    incognito: false,
+    thirdParty: false,
+    originUrl: '',
+    documentUrl: '',
+    frameId: 0,
+    parentFrameId: -1,
+    frameAncestors: [],
+    timeStamp: 1740215207080,
+    tabId: 4,
+    cookieStoreId: 'firefox-default',
+    urlClassification: {
+      firstParty: [],
+      thirdParty: [],
+    },
+  };
+
+  it('should return true for extension IPv4 connection check', () => {
+    const details: RequestDetails = {
+      ...baseDetails,
+      url: 'https://ipv4.am.i.mullvad.net/json',
+      originUrl: 'moz-extension://8ad8e256-a9a0-4017-b302-1345ac426553/dist/options/index.html',
+      documentUrl: 'moz-extension://8ad8e256-a9a0-4017-b302-1345ac426553/dist/options/index.html',
+    };
+    expect(isExtConnCheck(details)).toBeTruthy();
+  });
+
+  it('should return true for extension IPv6 connection check', () => {
+    const details: RequestDetails = {
+      ...baseDetails,
+      url: 'https://ipv6.am.i.mullvad.net/json',
+      originUrl: 'moz-extension://8ad8e256-a9a0-4017-b302-1345ac426553/dist/options/index.html',
+      documentUrl: 'moz-extension://8ad8e256-a9a0-4017-b302-1345ac426553/dist/options/index.html',
+    };
+    expect(isExtConnCheck(details)).toBeTruthy();
+  });
+
+  it('should return true for extension DNS check', () => {
+    const details: RequestDetails = {
+      ...baseDetails,
+      url: 'https://c30d3da2-fc4f-4732-86cc-f233e1692eac.dnsleak.am.i.mullvad.net/',
+      originUrl: 'moz-extension://8ad8e256-a9a0-4017-b302-1345ac426553/dist/options/index.html',
+      documentUrl: 'moz-extension://8ad8e256-a9a0-4017-b302-1345ac426553/dist/options/index.html',
+    };
+    expect(isExtConnCheck(details)).toBeTruthy();
+  });
+
+  it('should return false for DNS check with the wrong base domain', () => {
+    const details: RequestDetails = {
+      ...baseDetails,
+      url: 'https://example.com#am.i.mullvad.net/',
+      originUrl: 'moz-extension://8ad8e256-a9a0-4017-b302-1345ac426553/dist/options/index.html',
+      documentUrl: 'moz-extension://8ad8e256-a9a0-4017-b302-1345ac426553/dist/options/index.html',
+    };
+    expect(isExtConnCheck(details)).toBeFalsy();
+  });
+
+  it('should return false for non-extension requests', () => {
+    const details: RequestDetails = {
+      ...baseDetails,
+      url: 'https://ipv4.am.i.mullvad.net/json',
+      documentUrl: 'https://example.com',
+      originUrl: 'https://example.com',
+    };
+    expect(isExtConnCheck(details)).toBeFalsy();
+  });
+
+  it('should return false for extension requests to other URLs', () => {
+    const details: RequestDetails = {
+      ...baseDetails,
+      url: 'https://example.com',
+      originUrl: 'moz-extension://8ad8e256-a9a0-4017-b302-1345ac426553/dist/options/index.html',
+      documentUrl: 'moz-extension://8ad8e256-a9a0-4017-b302-1345ac426553/dist/options/index.html',
+    };
+    expect(isExtConnCheck(details)).toBeFalsy();
   });
 });
