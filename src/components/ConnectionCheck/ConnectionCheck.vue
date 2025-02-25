32abe6e04fbe37b03624e6eaadf455b013381453
rui hildt <rui@mullvad.net>
2025-02-25 17:07:38 +0100

    Improve connection check display in popup

---
+++ src/components/ConnectionCheck/ConnectionCheck.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import { inject, ref } from 'vue';
+import { NCard, NCollapseTransition, NIcon } from 'naive-ui';
+
+import ConnectionDetails from '@/components/ConnectionCheck/ConnectionDetails.vue';
+import ConnectionLocation from '@/components/ConnectionCheck/ConnectionLocation.vue';
+import FeChevronDown from '@/components/Icons/FeChevronDown.vue';
+import FeChevronUp from '@/components/Icons/FeChevronUp.vue';
+import IconLabel from '@/components/IconLabel.vue';
+import WebTRCDetails from '@/components/ConnectionCheck/WebTRCDetails.vue';
+
+import { ConnectionKey, defaultConnection } from '@/composables/useConnection';
+import { DnsServer } from '@/composables/useCheckDnsLeaks';
+
+defineProps<{
+  dnsServers: DnsServer[];
+  isErrorDNS: boolean;
+  isLoadingDNS: boolean;
+}>();
+
+const { isLoading, isError } = inject(ConnectionKey, defaultConnection);
+
+const showDetails = ref(true);
+</script>
+
+<template>
+  <n-card :bordered="false" class="mb-2">
+    <div v-if="isLoading || isError">
+      <p class="mb-2">
+        <IconLabel v-if="isLoading" text="Checking connection" type="spinner" />
+        <IconLabel v-if="isError" text="Couldn't get connection details" type="warning" />
+      </p>
+    </div>
+
+    <div v-else>
+      <div>
+        <ConnectionLocation />
+        <n-icon size="20" class="cursor-pointer" @click="showDetails = !showDetails">
+          <FeChevronUp v-if="showDetails" />
+          <FeChevronDown v-else />
+        </n-icon>
+      </div>
+
+      <div>
+        <n-collapse-transition :show="showDetails">
+          <ConnectionDetails :dnsServers :isErrorDNS :isLoadingDNS />
+          <WebTRCDetails />
+        </n-collapse-transition>
+      </div>
+    </div>
+  </n-card>
+</template>
