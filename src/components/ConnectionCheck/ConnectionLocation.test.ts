e2cde9ca21ebbafc376089011ae76b7f64945ea4
rui hildt <rui@mullvad.net>
2025-02-25 17:07:38 +0100

    Improve connection check display in popup

--- src/components/ConnectionCheck/ConnectionLocation.test.ts
+++ src/components/ConnectionCheck/ConnectionLocation.test.ts
@@ -2,7 +2,7 @@ import { it, describe, expect } from 'vitest';
 
 import { ref } from 'vue';
 import { mount } from '@vue/test-utils';
-import ConnectionLocation from '@/components/ConnectionLocation/ConnectionLocation.vue';
+import ConnectionLocation from '@/components/ConnectionCheck/ConnectionLocation.vue';
 import { ConnectionKey } from '@/composables/useConnection';
 
 describe('ConnectionLocation.vue', function () {
