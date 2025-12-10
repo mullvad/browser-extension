22d4975da2c6dbf96ce298a963055523c6d00bd7
rui hildt <rui@mullvad.net>
2025-12-10 15:55:53 +0100

    Update icon to follow the app

---
+++ src/components/Icons/FeLock.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+    <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+  </svg>
+</template>
