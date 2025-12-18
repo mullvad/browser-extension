9fa073989b4a7151e6bbd95956660f07c0203e2d
Hank <hank@mullvad.net>
2025-12-18 08:14:33 +0100

    Create CityButton

---
+++ src/components/CityButton.test.ts
@@ -0,0 +1,18 @@
+import CityButton from '@/components/CityButton.vue';
+import { mount } from '@vue/test-utils';
+import { NButton } from 'naive-ui';
+
+describe('CityButton', () => {
+  it('should render a CityButton', () => {
+    const onClickCity = vi.fn();
+    const wrapper = mount(CityButton, { props: { city: 'Narnia', onClickCity } });
+
+    const buttons = wrapper.findAllComponents(NButton);
+    expect(buttons).toHaveLength(1);
+    expect(buttons[0].text()).toBe('Narnia');
+
+    buttons[0].trigger('click');
+    expect(onClickCity).toHaveBeenCalledWith('Narnia')
+    expect(wrapper.element).toMatchSnapshot()
+  });
+});
---
+++ src/components/CityButton.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { NButton } from 'naive-ui';
+
+defineProps<{ city: string; onClickCity: (city: string) => void }>();
+</script>
+
+<template>
+  <n-button quaternary @click.prevent="onClickCity(city)">
+    {{ city }}
+  </n-button>
+</template>
