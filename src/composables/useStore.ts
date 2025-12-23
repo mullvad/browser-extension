7951b94148a2ad31e2071d5377e6487834fb4f1c
Hank <hank@mullvad.net>
2025-12-23 10:39:31 +0100

    Let typescript infer type for useStore

--- src/composables/useStore.ts
+++ src/composables/useStore.ts
@@ -1,40 +1,27 @@
-import { Ref } from 'vue';
-
-import type { HistoryEntriesMap } from '@/composables/useProxyHistory/HistoryEntries.types';
 import type {
   ProxyDetails,
-  ProxyInfoMap,
-  ProxyInfo,
   ProxyDetailsMap,
+  ProxyInfo,
+  ProxyInfoMap,
 } from '@/helpers/socksProxy/socksProxy.types';
 import { Tab } from '@/helpers/browserExtension';
 
 import useBrowserStorageLocal from '@/composables/useBrowserStorageLocal';
 import { SocksProxy } from '@/helpers/socksProxy/socksProxies.types';
+import { HistoryEntriesMap } from '@/composables/useProxyHistory/HistoryEntries.types';
 
-export type Store = {
-  excludedHosts: Ref<string[]>;
-  flatProxiesList: Ref<SocksProxy[]>;
-  globalProxy: Ref<ProxyInfo>;
-  globalProxyDetails: Ref<ProxyDetails>;
-  historyEntries: Ref<HistoryEntriesMap>;
-  hostProxies: Ref<ProxyInfoMap>;
-  hostProxiesDetails: Ref<ProxyDetailsMap>;
-  optionsActiveTab: Ref<Tab>;
-  proxyAutoReload: Ref<boolean>;
-  randomProxyMode: Ref<boolean>;
-  webRTCStatus: Ref<boolean>;
-};
-
-const useStore = (): Store => {
-  const excludedHosts = useBrowserStorageLocal('excludedHosts', []);
-  const flatProxiesList = useBrowserStorageLocal('flatProxiesList', [] as SocksProxy[]);
-  const globalProxy = useBrowserStorageLocal('globalProxy', {} as ProxyInfo);
-  const globalProxyDetails = useBrowserStorageLocal('globalProxyDetails', {} as ProxyDetails);
-  const historyEntries = useBrowserStorageLocal('historyEntries', {});
-  const hostProxies = useBrowserStorageLocal('hostProxies', {});
-  const hostProxiesDetails = useBrowserStorageLocal('hostProxiesDetails', {});
-  const optionsActiveTab = useBrowserStorageLocal('optionsActiveTab', Tab.SETTINGS);
+const useStore = () => {
+  const excludedHosts = useBrowserStorageLocal<string[]>('excludedHosts', []);
+  const flatProxiesList = useBrowserStorageLocal<SocksProxy[]>('flatProxiesList', []);
+  const globalProxy = useBrowserStorageLocal<ProxyInfo>('globalProxy', {} as ProxyInfo);
+  const globalProxyDetails = useBrowserStorageLocal<ProxyDetails>(
+    'globalProxyDetails',
+    {} as ProxyDetails,
+  );
+  const historyEntries = useBrowserStorageLocal<HistoryEntriesMap>('historyEntries', {});
+  const hostProxies = useBrowserStorageLocal<ProxyInfoMap>('hostProxies', {});
+  const hostProxiesDetails = useBrowserStorageLocal<ProxyDetailsMap>('hostProxiesDetails', {});
+  const optionsActiveTab = useBrowserStorageLocal<Tab>('optionsActiveTab', Tab.SETTINGS);
   const proxyAutoReload = useBrowserStorageLocal('proxyAutoReload', false);
   const randomProxyMode = useBrowserStorageLocal('randomProxyMode', false);
   const webRTCStatus = useBrowserStorageLocal('webRTCStatus', true);
